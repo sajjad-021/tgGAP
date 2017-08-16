b1efaacadef13bda5f15cccebf2869e2877ea8b5
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-08-16 10:30:31 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -17,44 +17,79 @@ default = {
 };
 " > "$THIS_DIR"/data/bot/bot.config
 }
+install_luarocks() {
+ echo -e "\e[38;5;142mInstalling LuaRocks\e"
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.4.2 # Current stable
+
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
+   make build && make install
+cd ..
+  rm -rf luarocks
+}
+
+install_rocks() {
+echo -e "\e[38;5;105mInstall rocks service\e"
+rocks="luasocket luasec redis-lua lua-term serpent dkjson Lua-cURL multipart-post lanes xml fakeredis luaexpat luasec lbase64 luafilesystem lub lua-cjson feedparser serpent"
+    for rock in $rocks; do
+      ./.luarocks/bin/luarocks install $rock
+    done
+}
+  
+tg() {
+echo -e "\e[38;5;099minstall telegram-cli\e"
+    rm -rf ../.telegram-cli
+    wget https://valtman.name/files/telegram-cli-1222
+    mv telegram-cli-1222 tg
+    chmod 777 tg
+}
+  
+install2() {
+echo -e "\e[38;5;034mInstalling more dependencies\e"
+    sudo apt-get install screen -y
+    sudo apt-get install tmux -y
+    sudo apt-get install upstart -y
+    sudo apt-get install libstdc++6 -y
+    sudo apt-get install lua-lgi -y
+    sudo apt-get install libnotify-dev -y
+}
+
+py() {
+ sudo apt-get install python-setuptools python-dev build-essential 
+ sudo easy_install pip
+ sudo pip install redis
+}
 
 install() {
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get install g++-4.7 c++-4.7
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-sudo apt-get install screen
-sudo apt-get install tmux
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
- tar zxpf luarocks-2.2.2.tar.gz
- mv luarocks-2.2.2 luarocks
- rm -rf 'luarocks-2.2.2.tar.gz'
- cd luarocks
- ./configure; sudo make bootstrap
- sudo luarocks install luasocket
- sudo luarocks install luasec
- sudo luarocks install redis-lua
- sudo luarocks install lua-term
- sudo luarocks install serpent
- sudo luarocks install dkjson
- sudo luarocks install lanes
- sudo luarocks install Lua-cURL
- cd $home
- cd tgGap
-  wget https://valtman.name/files/telegram-cli-1222
-mv telegram-cli-1222 tg
-chmod +x tg
-  cd ..
- bot_config
+echo -e "\e[38;5;035mUpdating packages\e"
+   sudo apt-get update -y
+   sudo apt-get upgrade -y
+
+echo -e "\\e[38;5;129mInstalling dependencies\e"
+	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+	sudo apt-get install g++-4.7 -y c++-4.7 -y
+	sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+install2
+install_luarocks
+install_rocks
+py
+tg
+log
+echo -e "\e[38;5;046mInstalling packages successfully\033[0;00m"
 }
+
 
 if [ "$1" = "install" ]; then
   install
 else
-  while true; do
-  bot_config
-  ./tg -s ./data/bot/bot.lua -l 1 -E -c ./data/bot/bot.config -p default "$@"
-   sleep 3
+COUNTER=0
+  while [ $COUNTER -lt 5 ]; do
+       tmux kill-session -t script
+           tmux new-session -s script "./tg -s ./data/bot/bot.lua -l 1 -E -c ./data/bot/bot.config -p default "$@""
+        tmux detach -s script
+    sleep 1
   done
 fi
